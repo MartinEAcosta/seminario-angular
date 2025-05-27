bf59704cd84fc70495f915fcc8b183403a91fb78
Martin <acostamartin1270@gmail.com>
2025-05-27 19:16:41 -0300

    carrito en funcionamiento

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Course } from './course-list/Course';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  private courses : Map<Course,number> = new Map<Course,number>;
+  private _courses : Map<Course,number> = new Map<Course,number>;
+  private _itemsSubject : BehaviorSubject<Map<Course,number>> = new BehaviorSubject(this._courses);
+  public courses : Observable<Map<Course,number>> = this._itemsSubject.asObservable();
   
-  onAddToCart = ( course : Course ) : void => {
-    const hasItem : boolean = this.courses.has( course )
+  onAddToCart = ( course : Course ) : Map<Course,number>  => {
+    const hasItem : boolean = this._courses.has( course )
     if( hasItem ){
-      let quantity : number = this.courses.get( course ) ?? 0;
-      this.courses.set( course, quantity++ )
+      let quantity : number = this._courses.get( course ) ?? 0;
+      this._courses.set( course, quantity++ )
+      return this._courses;
     }
-    this.courses.set( course, 1 );
+    this._courses.set( course, 1 );
+    console.log(this._courses)
+
+    this._itemsSubject.next(this._courses);
+    
+    return this._courses;
+  }
+
+  getItemsOfCart = ( ) : Map<Course, number> => {
+    return new Map<Course,number>(this._courses);
   }
 
   
--- src/app/udemix-cart/udemix-cart.component.ts
+++ src/app/udemix-cart/udemix-cart.component.ts
@@ -1,29 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Course } from '../course-list/Course';
 @Component({
   selector: 'app-udemix-cart',
   standalone: false,
   templateUrl: './udemix-cart.component.html',
   styleUrl: './udemix-cart.component.scss'
 })
-export class UdemixCartComponent {
+export class UdemixCartComponent implements OnInit {
 
+  
   isCartOpen : boolean = false;
+  cartItems !: Map<Course, number>;
 
+  
+  constructor(private cartService : CartService ) {}
+
+  ngOnInit(): void {
+    this.cartService.courses.subscribe(data => {
+      this.cartItems = data;
+    })
+  }
+  
   onOpenCart = ( ) : void => {
-
     if( this.isCartOpen ) {
       return;
     }
-
     this.isCartOpen = !this.isCartOpen;
 
   }
 
   onCloseCart = ( ) : void => {
-    
     const clickedContainer = event?.target as HTMLElement;
-
     if( clickedContainer.classList.contains('overlay') ){
       this.isCartOpen = false;
     }
@@ -33,6 +41,5 @@ export class UdemixCartComponent {
   onUpQuantity = ( ) : void => {
     
   }
-
 
 }

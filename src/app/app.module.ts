dd700d845a3a70c5091382c9b4b873d2d3786353
Martin <acostamartin1270@gmail.com>
2025-05-29 15:53:45 -0300

    agrego notificación al agregar al carrito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './course-list/course-list.component';
 import { UdemixCartComponent } from './shared/udemix-cart/udemix-cart.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -11,6 +10,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { BtnBuyComponent } from './shared/buttons/btn-buy/btn-buy.component';
 import { BtnAddToCartComponent } from './shared/buttons/btn-add-to-cart/btn-add-to-cart.component';
 import { AboutComponent } from './pages/about/about.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
 
 @NgModule({
   declarations: [

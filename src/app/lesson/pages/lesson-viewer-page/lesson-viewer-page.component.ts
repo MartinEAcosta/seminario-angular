f26e15a15140aca3c9b0fd3ff7038d10bd53f608
Martin <acostamartin1270@gmail.com>
2025-11-18 17:02:49 -0300

    preparando todo para la renderización de la multimedia del curso

--- src/app/lesson/pages/lesson-viewer-page/lesson-viewer-page.component.ts
+++ src/app/lesson/pages/lesson-viewer-page/lesson-viewer-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { Course } from '@course/models/course.interfaces';
 import { ListOfContentComponent } from "@lesson/components/list-of-content/list-of-content.component";
+import { ModuleService } from '@module/services/module.service';
+import { VjsPlayerComponent } from "@shared/components/vjs-player/vjs-player.component";
 
 @Component({
   selector: 'app-lesson-viewer-page',
-  imports: [ ListOfContentComponent ],
+  imports: [ListOfContentComponent, VjsPlayerComponent],
   templateUrl: './lesson-viewer-page.component.html',
   styleUrl: './lesson-viewer-page.component.scss'
 })
 export class LessonViewerPageComponent {
   
+  moduleService = inject(ModuleService);
   resolvedCourse = input.required<Course>();
+
+  modulesResource = rxResource({
+    request : () => ({ courseId : this.resolvedCourse().id }),
+    loader  : ( {request} ) => {
+      return this.moduleService.getModulesByCourseId( request.courseId );
+    },
+  });
 
 }
---
+++ src/app/shared/components/vjs-player/vjs-player.component.ts
@@ -0,0 +1,45 @@
+import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import videojs from 'video.js';
+import Player from 'video.js/dist/types/player';
+
+@Component({
+  selector: 'app-vjs-player',
+  templateUrl: './vjs-player.component.html',
+  styleUrls: [  ],
+  encapsulation: ViewEncapsulation.None,
+})
+
+export class VjsPlayerComponent implements OnInit, OnDestroy {
+  @ViewChild('target', {static: true}) target!: ElementRef;
+
+  // See options: https://videojs.com/guides/options
+  @Input() options!: {
+      fluid: boolean,
+      aspectRatio: string,
+      autoplay: boolean,
+      sources: {
+          src: string,
+          type: string,
+      }[],
+  };
+
+  player!: Player;
+
+  constructor(
+    private elementRef: ElementRef,
+  ) {  }
+
+  // Instantiate a Video.js player OnInit
+  ngOnInit() {
+    this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
+  }
+
+  // Dispose the player OnDestroy
+  ngOnDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+}

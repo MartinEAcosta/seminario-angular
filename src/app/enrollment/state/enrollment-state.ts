fa1531f6ec1b921404b486f0b9ec818da0fe9f77
Martin <acostamartin1270@gmail.com>
2025-12-11 20:14:24 -0300

    +enrollmentState, state.interface, enrollmentstatedata, +setIsloading en state,handle error state

--- src/app/enrollment/state/enrollment-state.ts
+++ src/app/enrollment/state/enrollment-state.ts
@@ -1,9 +1,16 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { catchError, finalize, Observable, of, tap } from 'rxjs';
 
 import { AuthService } from '@auth/services/auth.service';
 import { EnrollmentDetailed } from '@enrollment/models/enrollment.interfaces';
 import { EnrollmentService } from '@enrollment/services/enrollment.service';
+
+interface EnrollmentStateData {
+  isLoading : boolean,
+  error : string | null,
+  selectedEnrollment : EnrollmentDetailed | null,
+  enrollmentList : EnrollmentDetailed[] | null,
+};
 
 @Injectable({
   providedIn: 'root'
@@ -11,37 +18,102 @@ import { EnrollmentService } from '@enrollment/services/enrollment.service';
 export class EnrollmentState {
 
   private authService = inject(AuthService);
-  private _enrollmentList = signal<EnrollmentDetailed[] | null>(null);
-  private _selectedEnrollment = signal<EnrollmentDetailed | null>(null);
+  private enrollmentService = inject(EnrollmentService);
 
-  enrollmentList = this._enrollmentList.asReadonly();
-  selectedEnrollment = this._selectedEnrollment.asReadonly();
+  private state = signal<EnrollmentStateData>({
+    isLoading : false,
+    error : null,
+    selectedEnrollment : null,
+    enrollmentList : null,
+  });
+
+  enrollmentList = computed(() => this.state().enrollmentList);
+  selectedEnrollment = computed(() => this.state().selectedEnrollment);
   user = computed(() => this.authService.user());
 
-  constructor( private enrollmentService : EnrollmentService ) { }
+  constructor( ) {
+    effect(() => {
+      if (!this.user()) {
+        this.resetState();
+      }
+    });
+  }
 
-  obtainEnrollmentList = ( ) : Observable<EnrollmentDetailed[]> => {
+  obtainEnrollmentList ( ) : Observable<EnrollmentDetailed[]> {
     if( !this.user() ) return of([]);
+
     if( this.enrollmentList() ) { 
       return of( this.enrollmentList()! );
     }
+
+    this.setIsLoading(true);
+
     return this.enrollmentService.getEnrollmentsByUserId( this.user()!.id ).pipe( 
       tap( (enrollments) => {
-        this._enrollmentList.set( enrollments );
-      })
+        this.state.update( (c) => ({ ...c, enrollmentList : enrollments}));
+      }),
+      catchError( (error) => {
+        this.handleError( error );
+        throw error;
+      }),
+      finalize(() => this.setIsLoading(false))
     );
   }
 
-  obtainEnrollment = ( id_enrollment : string ) : Observable<EnrollmentDetailed> => {
+  obtainEnrollment ( id_enrollment : string ) : Observable<EnrollmentDetailed> {
     if( !this.user() ) return of();
-    if( this.selectedEnrollment() ){
+    
+    if( this.selectedEnrollment() && (this.selectedEnrollment()?.id === id_enrollment)  ){
       return of( this.selectedEnrollment()! );
     }
-    return this.enrollmentService.getEnrollmentPopulatedById( id_enrollment ).pipe(
-      tap( (enrollment) => {
-        this._selectedEnrollment.set( enrollment );
-      })
+
+    this.setIsLoading(true);
+
+    return this.enrollmentService
+      .getEnrollmentPopulatedById(id_enrollment)
+      .pipe(
+        tap((enrollment) => {
+          this.state.update((c) => ({
+            ...c,
+            selectedEnrollment: enrollment,
+            error: null,
+          }));
+        }),
+        catchError((error) => {
+          this.handleError(error);
+          throw error;
+        }),
+        finalize(() => this.setIsLoading(false))
+      );
+  }
+
+  setIsLoading( bool : boolean ) {
+    this.state.update( (c) => ({
+        ...c ,
+        isLoading : bool,
+    }))
+  }
+
+  resetState ( ) : EnrollmentStateData {
+    this.state.set( {
+      isLoading : false,
+      error : null,
+      enrollmentList : null,
+      selectedEnrollment : null,
+    } );
+
+    return this.state();
+  }
+
+  handleError ( error : any ) : EnrollmentStateData {
+    const errorMessage = error.message || 'Ocurrio un error inesperado.';
+
+    this.state.update( (c) => ({
+      ...c , 
+      error : errorMessage, 
+      }) 
     );
+    return this.state();
   }
 
 }

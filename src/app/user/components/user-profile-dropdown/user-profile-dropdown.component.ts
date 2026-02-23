e4d52bf7636dbb070268f61a2295f88e578ea429
Martin <acostamartin1270@gmail.com>
2026-02-23 12:24:23 -0300

    +userprofile en header +user-profile-dropdownCompnent

--- src/app/user/components/user-profile-dropdown/user-profile-dropdown.component.ts
+++ src/app/user/components/user-profile-dropdown/user-profile-dropdown.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-user-profile-dropdown',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './user-profile-dropdown.component.html',
   styleUrl: './user-profile-dropdown.component.scss'
 })
 export class UserProfileDropdownComponent {
+
+  authService = inject(AuthService);
+  open = signal<boolean>(true);
+
+  constructor ( ) { }
+
+  ngOnDestroy () {
+    this.open.set(false);
+  }
+
+  onClickProfile () {
+    this.open.update( open => !open );
+  }
 
 }

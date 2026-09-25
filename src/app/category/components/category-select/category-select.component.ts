1b55c43cd02c1687c489c584783125858d93cc07
Martin <acostamartin1270@gmail.com>
2026-09-25 13:40:42 -0300

    search de proyecto tipo serena

--- src/app/category/components/category-select/category-select.component.ts
+++ src/app/category/components/category-select/category-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, EventEmitter, inject, input, Input, Output, signal } from '@angular/core';
+import { Component, effect, ElementRef, EventEmitter, HostListener, inject, input, Output, signal } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { Category } from '../../models/category.interfaces';
@@ -12,15 +12,18 @@ import { Course } from '@course/models/course.interfaces';
   styleUrl: './item-select.component.scss'
 })
 export class CategorySelectComponent {
-  
-  private categoryService = inject(CategoryService);
 
-  @Output() 
+  private categoryService = inject(CategoryService);
+  private elementRef = inject(ElementRef);
+
+  @Output()
   clickCategory = new EventEmitter<string>();
   course = input.required<Course | null>();
 
+  open = signal<boolean>( false );
+  searchText = signal<string>( '' );
   categorySelected = signal<Category | undefined>( undefined );
-  categoriesResource = rxResource({ 
+  categoriesResource = rxResource({
     stream : () => { return this.categoryService.getAllCategories() }
   });
 
@@ -28,24 +31,50 @@ export class CategorySelectComponent {
     effect( () => {
       if( this.categoriesResource.hasValue() ) {
         const initialCategory = this.categoriesResource.value()
-                                                        .find( 
-                                                                category => 
-                                                                        category.id === this.course()?.id_category 
+                                                        .find(
+                                                                category =>
+                                                                        category.id === this.course()?.id_category
                                                               )
         this.categorySelected.set(initialCategory)
       }
     })
   }
 
+  get filteredCategories () : Category[] {
+    if( !this.categoriesResource.hasValue() ) return [];
+    const query = this.searchText().trim().toLowerCase();
+    if( !query ) return this.categoriesResource.value();
+    return this.categoriesResource.value().filter( category => category.name.toLowerCase().includes(query) );
+  }
+
+  public toggle ( ) : void {
+    this.open.update( open => !open );
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick ( event : MouseEvent ) : void {
+    if( this.open() && !this.elementRef.nativeElement.contains(event.target as Node) ) {
+      this.open.set(false);
+    }
+  }
+
+  public onSearchInput ( searchText : string ) : void {
+    this.searchText.set(searchText);
+  }
+
   public onSelectCategory ( category : Category ) : Category {
     this.clickCategory.emit(category.id);
     this.categorySelected.set(category);
+    this.open.set(false);
+    this.searchText.set('');
     return category;
   }
 
   public onClearCategory ( ) : void {
     this.categorySelected.set(undefined);
     this.clickCategory.emit('');
+    this.open.set(false);
+    this.searchText.set('');
   }
 
 }
--- src/app/course/components/form-course/form-course.component.ts
+++ src/app/course/components/form-course/form-course.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { CourseFormState } from '../../state/course-form/course-form-state';
@@ -19,7 +19,20 @@ export class FormCourseComponent {
 
   course = input.required<Course | null>();
 
+  settled = signal<boolean>( !this.courseFormState.isFormCollapsed() );
+
   constructor ( ) { }
+
+  public onToggleCollapse ( ) : void {
+    this.settled.set(false);
+    this.courseFormState.toggleFormCollapsed();
+  }
+
+  public onCollapseTransitionEnd ( event : TransitionEvent ) : void {
+    if( event.propertyName === 'grid-template-rows' && !this.courseFormState.isFormCollapsed() ) {
+      this.settled.set(true);
+    }
+  }
 
   ngOnInit(): void {
     const course = this.course();
